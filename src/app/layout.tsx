960a9fa4643cea05760076dd2697a40f5a0bba21
Paurakh <pandeypaurakhraj@gmail.com>
2025-08-19 17:08:03 +0545

    socket chat providerin root-layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "sonner";
 import Script from 'next/script';
 import { TranslateProvider } from "@/components/shared/TranslateProvider";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ChatProvider } from "@/context/ChatContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -237,10 +238,12 @@ export default function RootLayout({
         </Script>
         
         <ClerkProvider>
-          <TranslateProvider>
-            {children}
-          </TranslateProvider>
-          <Toaster position="top-right" richColors />
+          <ChatProvider>
+            <TranslateProvider>
+              {children}
+            </TranslateProvider>
+            <Toaster position="top-right" richColors />
+          </ChatProvider>
         </ClerkProvider>
       </body>
     </html>
